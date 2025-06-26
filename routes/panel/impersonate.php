190ccdc94c6a1829da4f3e40e4b44b2203a7c50b
Taufiq Pop <taufiqpop999@gmail.com>
2025-06-26 13:16:15 +0700

    my-app

--- routes/panel/impersonate.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\ImpersonateController;
-
-Route::get('/start/{id}', [ImpersonateController::class, 'start'])->name('impersonate.start');
-Route::get('/stop', [ImpersonateController::class, 'stop'])->name('impersonate.stop');
